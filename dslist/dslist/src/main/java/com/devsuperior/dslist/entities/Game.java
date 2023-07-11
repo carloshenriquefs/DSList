9f68fed012e77abcbd47f111a3788322cd683150
Carlos Henrique <ichfs9108@gmail.com>
2023-07-11 00:40:38 -0300

    Game repository, service, controller

--- dslist/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,23 +20,28 @@ public class Game {
 
 	@Column(name = "game_year")
 	private Integer year;
-	private String gener;
-	private String platform;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
-		super();
 	}
 
-	public Game(Long id, String title, Integer year, String gener, String platform, String imgUrl,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
+			String imgUrl,String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gener = gener;
-		this.platform = platform;
+		this.genre = genre;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -66,20 +71,28 @@ public class Game {
 		this.year = year;
 	}
 
-	public String getGener() {
-		return gener;
+	public String getGenre() {
+		return genre;
 	}
 
-	public void setGener(String gener) {
-		this.gener = gener;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
